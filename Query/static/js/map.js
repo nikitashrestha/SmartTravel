02a9dd0ccf0e3d51ab625ed490e5e2e6a495e15b
Shristi Shrestha <sthashristi777@gmail.com>
2018-06-03 22:38:24 +0545

    map page added

---
+++ Query/static/js/map.js
@@ -0,0 +1,9 @@
+function MyMap()
+{
+    var map_properties={
+        center: new google.maps.LatLng(51.508742,-0.120850),
+        zoom:13,
+    };
+    var map= new google.maps.Map(document.getElementById('map'), map_properties);
+    document.getElementById('check').innerHTML="Hhahahha";
+}
