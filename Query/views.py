d7106f04784b34b35b3c6b997b0597c54264ae60
Shristi Shrestha <sthashristi777@gmail.com>
2018-06-03 13:21:25 +0545

    further

--- Query/views.py
+++ Query/views.py
@@ -3,13 +3,13 @@ from django.views.generic import TemplateView
 from .models import query
 
 class index(TemplateView):
-    template_name="Query/index.html"
+    template_name="Query/search_page.html"
     q=query()
     def get(self, request):
-        return render(request, self.template_name, {'query': self.q})
+        return render(request, self.template_name, {'query': q})
 
 class submit(TemplateView):
-    template_name="Query/get_query_result.html"
+    template_name="Query/index.html"
     def get(self, request):
-        result=query(request.GET)
-        return render(request, self.template_name, {'result': result})
+        result = query(request.GET)
+        return render(request, self.template_name, {'res': result})
