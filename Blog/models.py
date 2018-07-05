0efa02dea2e74c07ef5474bef5df4c1a31da1d7d
ShishirBhandari <shishirbhandari54@gmail.com>
2018-07-05 16:13:49 +0545

    blogpost basic viewing
    We can add blogs and comments from admin interface and they are displayed in website.

--- Blog/models.py
+++ Blog/models.py
@@ -1,2 +1,31 @@
 from django.db import models
 
+# Create your models here.
+
+class BlogPost(models.Model):
+    ''''''
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_pub = models.DateTimeField(auto_now_add=True)
+    likes = models.PositiveIntegerField(default=0)
+    dislikes = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        '''Returns string representation of BlogPost model'''
+        return self.title[:50]
+
+class BlogComment(models.Model):
+    content = models.TextField(max_length=200)
+    date_pub = models.DateTimeField(auto_now_add=True)
+    blogPost = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
+
+    def __str__(self):
+        '''Returns string representation of BlogComment model'''
+        return self.content[:50]
+
+
+
+class Blog_Search(models.Model):
+    search_name=models.CharField(max_length=30) # kaslai search gareko
+    viewer_id=models.IntegerField() # kasle search gareko
+    searched_date = models.DateTimeField(auto_now_add=True)
--- Database/admin.py
+++ Database/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
-#admin.site.register(Blog_Search)
-#admin.site.register(Query)
-#admin.site.register(Blog)
-#admin.site.register(Photos)
-#admin.site.register(Result)
+
+admin.site.register(User_Profile)
+admin.site.register(Photos)
+admin.site.register(Result)
+admin.site.register(Blog)
+admin.site.register(Query)
+admin.site.register(ResultComment)
